7af5e35243d7081ae9c92e66aba45d1e58dcf52c
DionisKarpov <denis@orientedsoft.com>
2025-12-09 21:23:07 +0200

    fix modal container

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -24,7 +24,7 @@ const Modal = ({ isOpen, onClose, children }) => {
           onClick={onClose}>
       <div className="bg-[#FBF9F9] flex flex-col items-center justify-center rounded-[16px] max-w-2xl w-full max-h-[80vh] overflow-hidden py-10 px-8" 
            onClick={(e) => e.stopPropagation()}>
-        <div className="p-6 bg-white overflow-y-auto h-[452px] rounded-[16px]">
+        <div className="p-6 bg-white overflow-y-auto h-[452px] max-w-[calc(100vw-96px)] rounded-[16px]">
           {children}
         </div>
         <button onClick={onClose} className="p-4 w-[300px] hover:bg-[#05444F] text-white text-[20px] font-medium leading-[125%] 
